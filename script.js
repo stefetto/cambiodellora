08e7869b3028f29268780fd5af2dd62520f708e9
stefetto <antonini.s@alice.it>
2025-10-12 23:34:21 +0200

    Update script.js
    Per aprire il menu dai file articoli

--- script.js
+++ script.js
@@ -47,3 +47,13 @@ function updateCountdown() {
 }
 
 setInterval(updateCountdown, 1000);
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".nav-menu");
+
+    toggle.addEventListener("click", function () {
+      menu.classList.toggle("active");
+    });
+  });
+</script>
