fd5fde4c2cd7d9a071f88d8aafd69cd66cf559e6
stefetto <antonini.s@alice.it>
2025-10-12 23:37:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,13 +47,3 @@ function updateCountdown() {
 }
 
 setInterval(updateCountdown, 1000);
-<script>
-  document.addEventListener("DOMContentLoaded", function () {
-    const toggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".nav-menu");
-
-    toggle.addEventListener("click", function () {
-      menu.classList.toggle("active");
-    });
-  });
-</script>
